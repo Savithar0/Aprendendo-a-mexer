509de869e4f4524faf9a35c3fde7d5fd4048bc71
Savithar0 <64938762+Savithar0@users.noreply.github.com>
2020-05-07 00:06:09 -0300

    eu aprendi
    eu fiz um quadrado vermelho

---
+++ aplication/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div>texto{{variavel}}</div>
+    <div>
+  
+     <div :style="{backgroundColor: color}" class="quadradovermelho" v-if="variavel==='texto'"></div>
+     senha=texto<input type="text" v-model="variavel"> 
+     cor:<input type="text" v-model="color"> 
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    color:'green',
+    variavel: ''
+  })
+}
+</script>
+<style scoped>
+.quadradovermelho{
+  width: 500px;
+  height: 10px;
+}
+</style>
