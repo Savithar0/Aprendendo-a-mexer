854738aa4d11232d0af9151d6045fe1a10c1160f
Savithar0 <64938762+Savithar0@users.noreply.github.com>
2020-05-28 00:10:31 -0300

    adicionei vuetefy remodelando a pagina sobre e adicionando botões

--- aplication/src/router/index.js
+++ aplication/src/router/index.js
@@ -11,13 +11,13 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/af',
+    name: 'Af',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (af.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      return import(/* webpackChunkName: "af" */ '../views/Af.vue')
     }
   }
 ]
---
+++ aplication/src/views/Af.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="af">
+    <h1>Aplicativo Financeiro</h1>
+    <div class="geral">
+      <div class="botoes">
+        <v-btn x-large color="success" dark>Adicionar Salário</v-btn>
+        <v-btn x-large color="success" dark>Adicionar Salário</v-btn>
+        <v-btn x-large color="success" dark>Adicionar Salário</v-btn>
+        <v-btn x-large color="success" @click="window.close()" dark>Fechar</v-btn>
+      </div>
+    </div>
+  </div>
+</template>
