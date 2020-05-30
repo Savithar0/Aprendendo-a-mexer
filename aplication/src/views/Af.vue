e5e39b10e1526cec467aa1eef5fd8784ec981853
Savithar0 <64938762+Savithar0@users.noreply.github.com>
2020-05-29 22:44:12 -0300

    adicionei espaço nos botões

--- aplication/src/views/Af.vue
+++ aplication/src/views/Af.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="af">
+    {{contador}}
     <h1>Aplicativo Financeiro</h1>
     <div class="geral">
       <div class="botoes">
-        <v-btn x-large color="success" dark>Adicionar Salário</v-btn>
-        <v-btn x-large color="success" dark>Adicionar Salário</v-btn>
-        <v-btn x-large color="success" dark>Adicionar Salário</v-btn>
-        <v-btn x-large color="success" @click="window.close()" dark>Fechar</v-btn>
+        <v-btn x-large color="sucess" @click="contador++" dark>Adicionar Salário</v-btn>
+        <div>
+          <br />
+          <v-btn x-large color="success" @click="despesa(1)" dark>Despesa</v-btn>
+          <div>
+            <br />
+            <v-btn x-large color="success" dark>Crédito</v-btn>
+          </div>
+          <br />
+          <v-btn x-large color="success" @click="window.frames.closewindow()" dark>Fechar</v-btn>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+
+export default {
+  data: () => ({
+    contador: 0
+  }),
+  methods: {
+    despesa(valor) {
+    
+    }
+  }
+};
+</script>
