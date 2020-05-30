ed4427eba85856c341dcd868e59e6ba3c244a5d2
Savithar0 <64938762+Savithar0@users.noreply.github.com>
2020-05-30 00:23:37 -0300

    adicionei formulários

--- aplication/src/views/Af.vue
+++ aplication/src/views/Af.vue
@@ -4,16 +4,35 @@
     <h1>Aplicativo Financeiro</h1>
     <div class="geral">
       <div class="botoes">
-        <v-btn x-large color="sucess" @click="contador++" dark>Adicionar Salário</v-btn>
-        <div>
+        <v-btn x-large color="sucess" @click="mostrarbarraocultarsalario()" dark>{{botaosalario}}</v-btn>
+        <br />
+        <br />
+        <v-btn x-large color="success" dark>Despesa</v-btn>
+        <br />
+        <br />
+        <v-btn x-large color="success" dark>Crédito</v-btn>
+        <br />
+        <br />
+        <v-btn x-large color="success" @click="window.frames.closewindow()" dark>Fechar</v-btn>
+      </div>
+      <div class="campus">
+        <div class="salario" v-if="mostrarcampodosalario">
+          <h1>Salário</h1>
+          <input placeholder="valor" />
           <br />
-          <v-btn x-large color="success" @click="despesa(1)" dark>Despesa</v-btn>
-          <div>
-            <br />
-            <v-btn x-large color="success" dark>Crédito</v-btn>
-          </div>
+          <input placeholder="Descrição" />
+        </div>
+        <div class="despesa" v-if="mostrarcampodespesa">
+          <h1>Despesas</h1>
+          <input placeholder="valor" />
           <br />
-          <v-btn x-large color="success" @click="window.frames.closewindow()" dark>Fechar</v-btn>
+          <input placeholder="Descrição" />
+        </div>
+        <div class="credito" v-if="mostrarcampocredito">
+          <h1>Crédito</h1>
+          <input placeholder="valor" />
+          <br />
+          <input placeholder="Descrição" />
         </div>
       </div>
     </div>
@@ -24,12 +43,38 @@
 
 export default {
   data: () => ({
+    mostrarcampodosalario: false,
+    mostrarcampodespesa: false,
+    mostrarcampocredito: false,
     contador: 0
   }),
   methods: {
-    despesa(valor) {
-    
+    mostrarbarraocultarsalario() {
+      if (this.mostrarcampodosalario == false) {
+        this.mostrarcampodosalario = true;
+      } else {
+        this.mostrarcampodosalario = false;
+      }
+    }
+  },
+  computed: {
+    botaosalario() {
+      if (this.mostrarcampodosalario == false) {
+        return "Adicionar salário"
+      } else {
+        return "Fechar"
+      }
     }
   }
 };
 </script>
+<style  scoped>
+input {
+  margin-top: 2px;
+  border: solid 1px;
+  border-color: blue;
+}
+.campus {
+  text-align: center;
+}
+</style>
