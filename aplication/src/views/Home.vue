3bc9ca668f7862554eae4da3070f6d0d6bc5d74c
Savithar0 <64938762+Savithar0@users.noreply.github.com>
2020-05-29 22:43:43 -0300

    arrumei o codigo

--- aplication/src/views/Home.vue
+++ aplication/src/views/Home.vue
@@ -2,31 +2,31 @@
   <div class="home">
     <div>texto{{variavel}}</div>
     <div>
-  
-     <div :style="{backgroundColor: color}" class="quadradovermelho" v-if="variavel==='texto'"></div>
-     senha=texto<input type="text" v-model="variavel"> 
-     cor:<input type="text" v-model="color"> 
+      <div :style="{backgroundColor: color}" class="quadradovermelho" v-if="variavel==='texto'"></div>senha=texto
+      <input type="text" v-model="variavel" />
+      cor:
+      <input type="text" v-model="color" />
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
   },
   data: () => ({
-    color:'green',
-    variavel: ''
+    color: "green",
+    variavel: ""
   })
-}
+};
 </script>
 <style scoped>
-.quadradovermelho{
+.quadradovermelho {
   width: 500px;
   height: 10px;
 }
